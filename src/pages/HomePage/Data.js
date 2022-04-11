3f6173a8d28839cee31d628373a611cf2196a31e
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-04-11 15:59:53 -0500

    project initialized

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'GETA CLUB is emerging as the Esports platform.',
+  headline: 'ESports platform and videogames GETA CLUB',
+  description:
+    'At GETA club you will be able to invite players of all nationalities and locations, generating world tournaments with the best prizes and recognitions.',
+  buttonLabel: 'Get Started',
+  imgStart: '',
+  img: require('../../images/svg-5.svg'),
+  alt: 'Credit Card',
+  start: ''
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Our Vision',
+  headline: 'Make Shocking Services and Products',
+  description:
+    "We will make our products and services impact by improving and facilitating the lives of our clients through our competitiveness, innovation and customization, thus becoming a STANDARD. Achieving high satisfaction and good service to our customers.",
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: require('../../images/svg-7.svg'),
+  alt: 'Vault',
+  start: ''
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unique and Highly memorable experience',
+  headline:
+    'Growing group of interdisciplinary professionals',
+  description:
+    "Who contribute all their talent and experience to combine the impact of the most recognized influencers in the industry and the best games achieving a unique and highly memorable experience.",
+  buttonLabel: 'View Case Study',
+  imgStart: 'start',
+  img: require('../../images/svg-6.svg'),
+  alt: 'Vault',
+  start: 'true'
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'All your data is stored on our servers',
+  headline: 'Get Your Files Everywhere',
+  description:
+    'We store the files you want to add to our cloud service, thus you can access them from your computer or your phone.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: require('../../images/svg-3.svg'),
+  alt: 'Vault',
+  start: 'true'
+};
